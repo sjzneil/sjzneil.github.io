f04ba6c279b58831178c24380f254ada6305d1a1
sjzneil <sjzneil@foxmail.com>
2014-05-02 10:00:12 +0800

    beauty

---
+++ js/shop.js
@@ -0,0 +1,17 @@
+var store_items = [{
+    name: '蛋糕',
+    desc: '特价',
+    price: 99
+}, {
+    name: '蛋糕',
+    desc: '买二送一',
+    price: 99
+}, {
+    name: '蛋糕',
+    desc: '值得买',
+    price: 99
+}, {
+    name: '蛋糕',
+    desc: '吐血白菜价',
+    price: 99
+}, ];
